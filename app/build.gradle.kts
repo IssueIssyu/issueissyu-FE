46b0d9f69f8cd1e6d76ed6a9d4a3f0f26a897e69
dda-zi <sean061230@gmail.com>
2026-04-13 03:47:13 +0900

    bug: Sdk 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.issueissyu.fe"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.issueissyu.fe"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
