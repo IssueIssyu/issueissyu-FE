ca17abc0201e3e58221a0c4250b7ee5ff250753c
dda-zi <sean061230@gmail.com>
2026-04-22 16:33:29 +0900

    feat: 상단바 UI구성

--- app/src/main/java/com/issueissyu/fe/ui/components/IssueissyuTopAppBar.kt
+++ app/src/main/java/com/issueissyu/fe/ui/components/IssueissyuTopAppBar.kt
@@ -1,12 +1,18 @@
 package com.issueissyu.fe.ui.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.height
+import com.issueissyu.fe.ui.theme.White
+
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -26,6 +32,7 @@ fun IssueissyuTopAppBar(
                 Icon(
                     imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
                     contentDescription = "Back",
+                    modifier = Modifier.size(36.dp)
                 )
             }
         },
@@ -34,6 +41,8 @@ fun IssueissyuTopAppBar(
             titleContentColor = MaterialTheme.colorScheme.primary,
         ),
         modifier = modifier
+            .height(70.dp)
+            .padding(vertical = 10.dp) // Adjust vertical padding as needed
     )
 }
 
