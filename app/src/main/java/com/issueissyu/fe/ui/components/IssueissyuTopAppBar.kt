e7d6dc39be8215631d8acbec047f9c8abdcc0e50
dda-zi <sean061230@gmail.com>
2026-04-22 16:33:00 +0900

    feat: 상단바 UI구성

--- app/src/main/java/com/issueissyu/fe/ui/components/IssueissyuTopAppBar.kt
+++ app/src/main/java/com/issueissyu/fe/ui/components/IssueissyuTopAppBar.kt
@@ -3,8 +3,6 @@ package com.issueissyu.fe.ui.components
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,7 +12,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,7 +26,6 @@ fun IssueissyuTopAppBar(
                 Icon(
                     imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
                     contentDescription = "Back",
-                    modifier = Modifier.size(30.dp)
                 )
             }
         },
