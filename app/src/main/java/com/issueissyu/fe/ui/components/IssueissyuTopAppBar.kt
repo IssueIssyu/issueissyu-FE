e73fd244761bdcd32da3f7738d9249c5e478ff9d
dda-zi <sean061230@gmail.com>
2026-04-24 17:54:59 +0900

    bug: 안쓰이는 import 제거

--- app/src/main/java/com/issueissyu/fe/ui/components/IssueissyuTopAppBar.kt
+++ app/src/main/java/com/issueissyu/fe/ui/components/IssueissyuTopAppBar.kt
@@ -3,15 +3,12 @@ package com.issueissyu.fe.ui.components
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.height
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
