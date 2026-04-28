e25035679e453651db765332d95dce7cc03e2cbc
dda-zi <sean061230@gmail.com>
2026-04-28 22:20:19 +0900

    feat: 지도 화면 일부 구현 핀생성 선택 화면 등 차후 구현 필요

--- app/src/main/java/com/issueissyu/fe/ui/navigation/AppDestinations.kt
+++ app/src/main/java/com/issueissyu/fe/ui/navigation/AppDestinations.kt
@@ -5,5 +5,7 @@ object AppDestinations {
     const val COLLECTION_ROUTE = "collection_route"
     const val TOWN_ROUTE = "town_route"
     const val COMMUNITY_ROUTE = "community_route"
-    const val MYPAGE_ROUTE = "mypage_route" 
+    const val MYPAGE_ROUTE = "mypage_route"
+    const val PATCH_NOTE_ROUTE = "patch_note_route"
+    const val PIN_CREATION_ROUTE = "pin_creation_route"
 }
