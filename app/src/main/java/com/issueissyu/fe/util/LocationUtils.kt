6a1c73f8da9ce3cc29a35988e1af6f037192ee16
dda-zi <sean061230@gmail.com>
2026-05-05 16:32:31 +0900

    refactor: 리베이스 및 컨플릭트 관리

--- app/src/main/java/com/issueissyu/fe/util/LocationUtils.kt
+++ app/src/main/java/com/issueissyu/fe/util/LocationUtils.kt
@@ -2,51 +2,8 @@ package com.issueissyu.fe.util
 
 import android.location.Location
 import com.issueissyu.fe.data.model.PinCoordinate
-import com.naver.maps.geometry.LatLng
 
 fun PinCoordinate.distanceTo(other: PinCoordinate): Double {
-    val results = FloatArray(1)
-
-    Location.distanceBetween(
-        latitude,
-        longitude,
-        other.latitude,
-        other.longitude,
-        results
-    )
-
-    return results[0].toDouble()
-}
-
-fun PinCoordinate.distanceTo(other: LatLng): Double {
-    val results = FloatArray(1)
-
-    Location.distanceBetween(
-        latitude,
-        longitude,
-        other.latitude,
-        other.longitude,
-        results
-    )
-
-    return results[0].toDouble()
-}
-
-fun LatLng.distanceTo(other: LatLng): Double {
-    val results = FloatArray(1)
-
-    Location.distanceBetween(
-        latitude,
-        longitude,
-        other.latitude,
-        other.longitude,
-        results
-    )
-
-    return results[0].toDouble()
-}
-
-fun LatLng.distanceTo(other: PinCoordinate): Double {
     val results = FloatArray(1)
 
     Location.distanceBetween(
