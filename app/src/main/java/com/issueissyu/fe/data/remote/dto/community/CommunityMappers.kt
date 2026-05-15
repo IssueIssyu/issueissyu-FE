4c50399d729b49658bb5bde50cc8a8cb4599f502
dda-zi <sean061230@gmail.com>
2026-05-16 02:40:24 +0900

    feat: 커뮤니티 피드 API DTO 및 mapper 추가

---
+++ app/src/main/java/com/issueissyu/fe/data/remote/dto/community/CommunityMappers.kt
@@ -0,0 +1,38 @@
+package com.issueissyu.fe.data.remote.dto.community
+
+import com.issueissyu.fe.domain.model.community.CommunityFeed
+import com.issueissyu.fe.domain.model.community.CommunityFeedItem
+import com.issueissyu.fe.domain.model.community.CommunityItemKind
+
+fun CommunityFeedResponse.toCommunityFeed(): CommunityFeed {
+    return CommunityFeed(
+        items = this.content?.map { it.toCommunityFeedItem() } ?: emptyList(),
+        region = this.region ?: "",
+        nextCursor = this.nextCursor,
+        hasNext = this.hasNext ?: false
+    )
+}
+
+fun CommunityFeedItemResponse.toCommunityFeedItem(): CommunityFeedItem {
+    return CommunityFeedItem(
+        communityId = this.communityId ?: 0L, // TODO: communityId null 처리 정책 확정 필요
+        pinId = this.pinId,
+        kind = try {
+            this.kind?.let { CommunityItemKind.valueOf(it.uppercase()) } ?: CommunityItemKind.UNKNOWN
+        } catch (e: IllegalArgumentException) {
+            CommunityItemKind.UNKNOWN
+        },
+        title = this.title ?: this.pinTitle ?: "제목 없음",
+        content = this.content,
+        thumbnailUrl = this.pinImageUrl ?: this.thumbnailUrl ?: this.storeImageUrl,
+        writerNickname = this.pinUserNickname,
+        writerProfileUrl = this.pinUserProfile,
+        address = this.pinDetailAddress ?: this.address,
+        viewCount = this.viewCount ?: 0,
+        likeCount = this.likeCount ?: 0,
+        eventStartTime = this.eventStartTime,
+        eventEndTime = this.eventEndTime,
+        discount = this.discount,
+        isHot = false // TODO: isHot 필드 서버 응답 확정 시 처리 필요 (현재 false로 fallback)
+    )
+}
