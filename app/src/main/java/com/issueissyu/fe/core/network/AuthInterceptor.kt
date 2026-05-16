89a71c04d3f0324716758d50b9d28dac2bf25f15
kge <kge8601@gmail.com>
2026-05-17 02:26:29 +0900

    refactor: 경로 수정

--- app/src/main/java/com/issueissyu/fe/core/network/AuthInterceptor.kt
+++ app/src/main/java/com/issueissyu/fe/core/network/AuthInterceptor.kt
@@ -35,7 +35,7 @@ class AuthInterceptor @Inject constructor(
     private fun Request.shouldAttachAccessToken(): Boolean {
         val path = url.encodedPath
         if (path.endsWith("/auth/signup/local")) return false
-        if (path.contains("/api/auth/check/username/")) return false
+        if (path.contains("/auth/check/username/")) return false
         if (path.endsWith("/auth/login/local")) return false
         if (path.endsWith("/auth/login/naver")) return false
         if (path.endsWith("/auth/refresh")) return false
