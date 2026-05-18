d4b07d9549ded879ee37c1c6b57de0fc0066a742
kge <kge8601@gmail.com>
2026-05-18 14:11:06 +0900

    refactor: 컬렉션 다시 추가

--- app/src/main/java/com/issueissyu/fe/ui/IssueissyuApp.kt
+++ app/src/main/java/com/issueissyu/fe/ui/IssueissyuApp.kt
@@ -11,7 +11,8 @@ import com.issueissyu.fe.ui.navigation.AppNavGraph
 
 private fun shouldShowBottomBar(route: String?): Boolean {
     if (route == null) return false
-    return route == AppDestinations.TOWN_ROUTE ||
+    return route == AppDestinations.COLLECTION_ROUTE ||
+        route == AppDestinations.TOWN_ROUTE ||
         route == AppDestinations.COMMUNITY_ROUTE ||
         route == AppDestinations.MYPAGE_ROUTE ||
         route == AppDestinations.PATCH_NOTE_ROUTE ||
