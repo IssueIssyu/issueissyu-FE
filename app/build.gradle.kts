f14a2d3fc75bab8f7ecf867e41d1d7757d0e994e
dda-zi <sean061230@gmail.com>
2026-05-20 18:05:08 +0900

    bug: buildConfigString 함수 오류 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
                 ?: ""
 
         buildConfigField("String", "API_BASE_URL", "\"${localProperties.buildConfigString("API_BASE_URL", "api.base.url", "https://api.example.com/")}\"")
-        buildConfigField("String", "NAVER_CLIENT_ID", "\"${localProperties.buildConfigString("NAVER_CLIENT_ID", "naver.client.id")}\"")
-        buildConfigField("String", "NAVER_CLIENT_SECRET", "\"${localProperties.buildConfigString("NAVER_CLIENT_SECRET", "naver.client.secret")}\"")
+        buildConfigField("String", "NAVER_CLIENT_ID", "\"${localProperties.buildConfigString("NAVER_CLIENT_ID", "naver.client.id", "")}\"")
+        buildConfigField("String", "NAVER_CLIENT_SECRET", "\"${localProperties.buildConfigString("NAVER_CLIENT_SECRET", "naver.client.secret", "")}\"")
         buildConfigField("String", "NAVER_CLIENT_NAME", "\"${localProperties.buildConfigString("NAVER_CLIENT_NAME", "naver.client.name", "이슈있슈")}\"")
     }
 
